760ef700fade5d004a81a37089fcc2133eda4aaf
Lichunhui <1649273518@qq.com>
2021-07-20 14:14:51 +0800

    master second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("master first commit");
         System.out.println("Hot-fix first commit");
         System.out.println("Hot-fix first commit2");
+        System.out.println("master second commit");
     }
 }
